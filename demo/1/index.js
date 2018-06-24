ac79c587a7876783166ed247a99011ec1e601a86
guojun01 <guojun01@58.com>
2018-06-24 17:08:30 +0800

    first

---
+++ demo/1/index.js
@@ -0,0 +1,11 @@
+const CountStream = require('./countstream');
+const countStream = new CountStream();
+const http = require('http');
+
+http.get('http://www.manning.com', (res) => {
+  res.pipe(countStream);
+});
+
+countStream.on('total', (count) => {
+  console.log('Total matches:', count);
+});
---
+++ demo/1/test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const CountStream = require('./countstream');
+const countStream = new CountStream('test');
+const fs = require('fs');
+
+let passed = 0;
+countStream.on('total', (res) => {
+  assert.equal(res, 1);
+  passed++;
+});
+
+fs.createReadStream(__filename).pipe(countStream);
+
+process.on('exit', () => {
+  console.log('Assertions passed:', passed);
+})
+;
